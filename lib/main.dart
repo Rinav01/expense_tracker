f45767112acae1f8f9c5dd7bc7cac95ceec4e874
Rinav01 <23051451@kit.ac.in>
2024-10-08 00:08:02 +0530

    branch 1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -19,5 +20,6 @@ class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+    print("");
   }
 }
