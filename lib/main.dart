231c3143daee6aceefc3555b0f5bcfe7d2e2de52
Rinav01 <23051451@kit.ac.in>
2024-10-07 23:08:01 +0530

    Second

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
