3fcef6ca5177ff6f2f449c06391b5963d1323c68
Rinav01 <23051451@kit.ac.in>
2024-10-18 00:40:15 +0530

    Third Commit

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,57 +1,73 @@
 import 'dart:math';
-
 import 'package:expense_tracker/screens/home/views/main_screen.dart';
+import 'package:expense_tracker/screens/stats/stats.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  late Color selectedItem = Colors.blue;
+  Color unselectedItem = Colors.grey;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.vertical(
-          top: Radius.circular(30),
-        ),
-        child: BottomNavigationBar(
-            backgroundColor: Colors.white,
-            elevation: 3,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.graph_square_fill),
-                label: 'Charts',
-              ),
-            ]),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        shape: const CircleBorder(),
-        child: Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            gradient: LinearGradient(colors:[ 
-            Theme.of(context).colorScheme.primary,
-            Theme.of(context).colorScheme.secondary,
-            Theme.of(context).colorScheme.tertiary,
-          ],
-          transform: const GradientRotation(pi /4), 
-          )
+        // appBar: AppBar(),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: const BorderRadius.vertical(
+            top: Radius.circular(30),
           ),
-          child: const Icon(
-            CupertinoIcons.add
+          child: BottomNavigationBar(
+              onTap: (value) {
+                setState(() {
+                  index = value;
+                });
+              },
+              elevation: 3,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    CupertinoIcons.home,
+                    color: index == 0 ? selectedItem : unselectedItem,
+                  ),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    CupertinoIcons.graph_square_fill,
+                    color: index == 1 ? selectedItem : unselectedItem,
+                  ),
+                  label: 'Charts',
+                ),
+              ]),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          shape: const CircleBorder(),
+          child: Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).colorScheme.primary,
+                    Theme.of(context).colorScheme.secondary,
+                    Theme.of(context).colorScheme.tertiary,
+                  ],
+                  transform: const GradientRotation(pi / 4),
+                )),
+            child: const Icon(CupertinoIcons.add),
           ),
         ),
-      ),
-      body: const MainScreen(),
-    );
+        body: index == 0 ? MainScreen() : StatScreen());
   }
 }
