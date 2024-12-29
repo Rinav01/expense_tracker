1894f8e224eb99cacff73f9c6b51f7e2124893c5
Rinav01 <23051451@kit.ac.in>
2024-12-29 20:49:51 +0530

    sql changes reverted

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import '../../../data/data.dart';
 
 class MainScreen extends StatelessWidget {
   final List<Expense> expenses;
